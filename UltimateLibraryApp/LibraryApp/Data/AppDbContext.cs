54df1c87c26fbdfdbe266a5162968d328a818d0b
jvillad <jyvillad@gmail.com>
2023-03-18 00:15:29 +1100

    application db context for migration

---
+++ UltimateLibraryApp/LibraryApp/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LibraryApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApp.Data
+{
+	public class AppDbContext : IdentityDbContext
+	{
+		public DbSet<Book> Books { get; set; }
+		public DbSet<Genre> Genres { get; set; }
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+	}
+}
