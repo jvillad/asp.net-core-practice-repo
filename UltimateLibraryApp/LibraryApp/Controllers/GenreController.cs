981f454fa5f5169a4e8a2df729e1e8425de1b601
jvillad <jyvillad@gmail.com>
2023-03-18 00:14:28 +1100

    added a genre controller

---
+++ UltimateLibraryApp/LibraryApp/Controllers/GenreController.cs
@@ -0,0 +1,28 @@
+﻿using LibraryApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Controllers
+{
+	public class GenreController : Controller
+	{
+		private readonly AppDbContext _context;
+		public GenreController(AppDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			return View(_context.Genres.ToList());
+		}
+		//get action?
+		public IActionResult Create()
+		{
+			return View();
+		}
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
+	}
+}
