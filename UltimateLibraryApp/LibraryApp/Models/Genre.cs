a9a9eabe99dd3e6d2750996fbed4c5e0114bafac
jvillad <jyvillad@gmail.com>
2023-03-18 00:17:22 +1100

    update pk, range and added model for the individual button

--- UltimateLibraryApp/LibraryApp/Models/Genre.cs
+++ UltimateLibraryApp/LibraryApp/Models/Genre.cs
@@ -7,6 +7,7 @@ namespace LibraryApp.Models
 		[Required]
 		public int Id { get; set; }
 		[Required]
+		[MaxLength(50)]
 		public string Name { get; set; }
 	}
 }
---
+++ UltimateLibraryApp/LibraryApp/Models/IndividualButtonPartial.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace LibraryApp.Models
+{
+	public class IndividualButtonPartial
+	{
+		public string ButtonType { get; set; }
+		public string Action { get; set; }
+		public string Glyph { get; set; }
+		public string Text { get; set; }
+		public int? GenreId { get; set; }
+		public int? BookId { get; set; }
+		public int? MemberId { get; set; }
+		public int? MemberShipId { get; set; }
+		public string ActionParameter
+		{
+			get
+			{
+				var param = new StringBuilder("@/");
+				if (BookId != null && BookId > 0)
+				{
+					param.Append(String.Format("{0}", BookId));
+				}
+				if (GenreId != null && GenreId > 0)
+				{
+					param.Append(String.Format("{0}", GenreId));
+				}
+				if (MemberId != null && MemberId > 0)
+				{
+					param.Append(String.Format("{0}", MemberId));
+				}
+				if (MemberShipId != null && MemberShipId > 0)
+				{
+					param.Append(String.Format("{0}", MemberShipId));
+				}
+				return param.ToString();
+			}
+		}
+	}
+
+
+}
+
