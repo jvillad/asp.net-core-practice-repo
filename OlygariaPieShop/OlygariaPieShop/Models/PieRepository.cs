69e5ef36d75d66e2ac79e07700fb144181eaf9d9
jvillad <jyvillad@gmail.com>
2023-03-20 22:46:56 +1100

    new mode for Pie

---
+++ OlygariaPieShop/OlygariaPieShop/Models/PieRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OlygariaPieShop.Models
+{
+	public class PieRepository : IPieRepository
+	{
+		private readonly OlygariaPieShopDbContext _context;
+
+		public PieRepository(OlygariaPieShopDbContext context)
+		{
+			_context = context;
+		}
+
+		public IEnumerable<Pie> AllPies
+		{
+			get
+			{
+				return _context.Pies.Include(p => p.Category);
+			}
+		}
+
+		public IEnumerable<Pie> PiesOfTheWeek
+		{
+			get { return _context.Pies.Include(p => p.Category).Where(p => p.IsPieOfTheWeek); }
+		}
+
+		public Pie? GetPieById(int pieId)
+		{
+			return _context.Pies.FirstOrDefault(p => p.Id == pieId);
+		}
+	}
+}
