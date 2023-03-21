285953be77a9a31133094154c153207c5228adfd
jvillad <jyvillad@gmail.com>
2023-03-21 16:42:33 +1100

    home page controller

---
+++ OlygariaPieShop/OlygariaPieShop/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlygariaPieShop.Models;
+using OlygariaPieShop.ViewModels;
+
+namespace OlygariaPieShop.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly IPieRepository _pieRepository;
+
+		public HomeController(IPieRepository pieRepository)
+		{
+			_pieRepository = pieRepository;
+		}
+		public IActionResult Index()
+		{
+			var piesOfThWeek = _pieRepository.PiesOfTheWeek;
+			var homeViewModel = new HomeViewModel(piesOfThWeek);
+			return View(homeViewModel);
+		}
+	}
+}
