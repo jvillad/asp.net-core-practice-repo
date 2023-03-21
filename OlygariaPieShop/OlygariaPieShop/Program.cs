9e6dc238d90cea966d369ca4f8d0b0ed47455443
jvillad <jyvillad@gmail.com>
2023-03-21 16:50:38 +1100

    proj configs

--- OlygariaPieShop/OlygariaPieShop/Program.cs
+++ OlygariaPieShop/OlygariaPieShop/Program.cs
@@ -1,12 +1,23 @@
+using Microsoft.EntityFrameworkCore;
 using OlygariaPieShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+
+//invokes the GetCart method passing in the service provider(sp)
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
 
 // will make sure that application knows about ASP.Net Core MVC
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<OlygariaPieShopDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration["ConnectionStrings:AppDbContextConnection"]);
+});
 
 var app = builder.Build();
 
@@ -14,6 +25,8 @@ var app = builder.Build();
 // middleware component
 // files from wwwroot
 app.UseStaticFiles();
+
+app.UseSession();
 
 // middleware component
 // show errors
@@ -24,4 +37,10 @@ if (app.Environment.IsDevelopment())
 // endpoint middleware
 app.MapDefaultControllerRoute();
 
+/*app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");*/
+
+// seeding db
+DbSeedData.Seed(app);
 app.Run();
