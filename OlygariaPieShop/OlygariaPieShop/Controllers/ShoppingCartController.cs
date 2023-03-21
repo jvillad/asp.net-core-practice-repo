6484be8216a532d26a3e765fc5ff853dc93858b6
jvillad <jyvillad@gmail.com>
2023-03-21 16:43:51 +1100

    added shopping cart controller

---
+++ OlygariaPieShop/OlygariaPieShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlygariaPieShop.Models;
+using OlygariaPieShop.ViewModels;
+
+namespace OlygariaPieShop.Controllers
+{
+	public class ShoppingCartController : Controller
+	{
+		private readonly IPieRepository _pieRepository;
+		private readonly IShoppingCart _shoppingCart;
+
+		public ShoppingCartController(IPieRepository pieRepository, IShoppingCart shoppingCart)
+		{
+			_pieRepository = pieRepository;
+			_shoppingCart = shoppingCart;
+		}
+
+		public ViewResult Index()
+		{
+			var items = _shoppingCart.GetShoppingCartItems();
+			_shoppingCart.ShoppingCartItems = items;
+
+			var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
+			return View(shoppingCartViewModel);
+		}
+
+		public RedirectToActionResult AddToShoppingCart(int pieId)
+		{
+			var selectedPie = _pieRepository.AllPies.FirstOrDefault(p => p.Id == pieId);
+			if (selectedPie != null)
+			{
+				_shoppingCart.AddToCart(selectedPie);
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		public RedirectToActionResult RemoveFromShoppingCart(int pieId)
+		{
+			var selectedPie = _pieRepository.AllPies.FirstOrDefault(p => p.Id == pieId);
+			if (selectedPie != null)
+			{
+				_shoppingCart.RemoveFromCart(selectedPie);
+			}
+			return RedirectToAction("Index");
+		}
+
+	}
+}
