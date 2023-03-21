d7153768b6c5425274568e592d83fd57fc07cc56
jvillad <jyvillad@gmail.com>
2023-03-21 16:42:03 +1100

    Added pie category component

---
+++ OlygariaPieShop/OlygariaPieShop/Components/CategoryMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlygariaPieShop.Models;
+
+namespace OlygariaPieShop.Components
+{
+	public class CategoryMenu : ViewComponent
+	{
+		private readonly ICategoryRepository _categoryRepository;
+
+		public CategoryMenu(ICategoryRepository categoryRepository)
+		{
+			_categoryRepository = categoryRepository;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var categories = _categoryRepository.AllCategories.OrderBy(c => c.CategoryName);
+			return View(categories);
+		}
+	}
+}
