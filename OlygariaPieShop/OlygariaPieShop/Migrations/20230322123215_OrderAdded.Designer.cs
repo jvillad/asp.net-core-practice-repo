73d818149e8184df2de54b88d5640e3b4d6ae0da
jvillad <jyvillad@gmail.com>
2023-03-22 23:47:56 +1100

    Orders and OrderDetails migration

--- OlygariaPieShop/OlygariaPieShop/Migrations/20230322123215_OrderAdded.Designer.cs
+++ OlygariaPieShop/OlygariaPieShop/Migrations/20230322123215_OrderAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OlygariaPieShop.Models;
 
@@ -11,9 +12,11 @@ using OlygariaPieShop.Models;
 namespace OlygariaPieShop.Migrations
 {
     [DbContext(typeof(OlygariaPieShopDbContext))]
-    partial class OlygariaPieShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230322123215_OrderAdded")]
+    partial class OrderAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
