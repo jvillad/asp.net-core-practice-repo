0da2fef1154a8bedd28fa548ecfa7df14e4f23c5
jvillad <jyvillad@gmail.com>
2023-03-22 23:49:29 +1100

    added models(customer info,orderdetails) needed for the checkout

---
+++ OlygariaPieShop/OlygariaPieShop/Models/OrderRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace OlygariaPieShop.Models
+{
+	public class OrderRepository : IOrderRepository
+	{
+		private readonly OlygariaPieShopDbContext _olygariaPieShopDbContext;
+		private readonly IShoppingCart _shoppingCart;
+
+		public OrderRepository(OlygariaPieShopDbContext olygariaPieShopDbContext, IShoppingCart shoppingCart)
+		{
+			_olygariaPieShopDbContext = olygariaPieShopDbContext;
+			_shoppingCart = shoppingCart;
+		}
+
+		public void CreateOrder(Order order)
+		{
+			order.OrderPlaced = DateTime.Now;
+
+			List<ShoppingCartItem>? shoppingCartItems = _shoppingCart.ShoppingCartItems;
+			order.OrderTotal = _shoppingCart.GetShoppingCartTotal();
+
+			order.OrderDetails = new List<OrderDetail>();
+
+			//adding the order with its details
+
+			foreach (ShoppingCartItem? shoppingCartItem in shoppingCartItems)
+			{
+				var orderDetail = new OrderDetail
+				{
+					Amount = shoppingCartItem.Amount,
+					PieId = shoppingCartItem.Pie.Id,
+					Price = shoppingCartItem.Pie.Price
+				};
+
+				order.OrderDetails.Add(orderDetail);
+			}
+
+			_olygariaPieShopDbContext.Orders.Add(order);
+
+			_olygariaPieShopDbContext.SaveChanges();
+		}
+	}
+}
