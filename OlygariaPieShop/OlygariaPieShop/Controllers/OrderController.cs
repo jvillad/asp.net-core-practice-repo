c52c86e9439d828070b931c09a31f40584f1f7bc
jvillad <jyvillad@gmail.com>
2023-03-22 23:49:40 +1100

    Added order controller

---
+++ OlygariaPieShop/OlygariaPieShop/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlygariaPieShop.Models;
+
+namespace OlygariaPieShop.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IOrderRepository _orderRepository;
+		private readonly IShoppingCart _shoaderCart;
+		public OrderController(IOrderRepository orderRepository, IShoppingCart shoaderCart)
+		{
+			_orderRepository = orderRepository;
+			_shoaderCart = shoaderCart;
+		}
+
+		public ActionResult Checkout()
+		{
+			return View();
+		}
+	}
+}
