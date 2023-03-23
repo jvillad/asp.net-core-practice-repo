b0e11d9d46772332e6e501a70c1d29691a6787c3
jvillad <jyvillad@gmail.com>
2023-03-23 23:37:43 +1100

    update order controller for order checkout post with checkout complete message

--- OlygariaPieShop/OlygariaPieShop/Controllers/OrderController.cs
+++ OlygariaPieShop/OlygariaPieShop/Controllers/OrderController.cs
@@ -6,16 +6,45 @@ namespace OlygariaPieShop.Controllers
 	public class OrderController : Controller
 	{
 		private readonly IOrderRepository _orderRepository;
-		private readonly IShoppingCart _shoaderCart;
+		private readonly IShoppingCart _shoppingCart;
 		public OrderController(IOrderRepository orderRepository, IShoppingCart shoaderCart)
 		{
 			_orderRepository = orderRepository;
-			_shoaderCart = shoaderCart;
+			_shoppingCart = shoaderCart;
 		}
 
-		public ActionResult Checkout()
+		public ActionResult Checkout() //Get
 		{
 			return View();
 		}
+		// called when a post is received
+		[HttpPost]
+		public IActionResult Checkout(Order order)
+		{
+
+			var items = _shoppingCart.GetShoppingCartItems();
+			_shoppingCart.ShoppingCartItems = items;
+
+			if (_shoppingCart.ShoppingCartItems.Count == 0)
+			{
+				// warning
+				ModelState.AddModelError("", "Cart is empty, gotta get some yummy pie first!");
+			}
+
+			if (ModelState.IsValid)
+			{
+				_orderRepository.CreateOrder(order);
+				_shoppingCart.ClearCart();
+				return RedirectToAction("CheckoutComplete");
+			}
+
+			return View(order);
+		}
+		public IActionResult CheckoutComplete()
+		{
+			ViewBag.CheckoutCompleteMessage = "Order Complete! The supa delicious pie is on your way!";
+			return View();
+		}
+
 	}
 }
