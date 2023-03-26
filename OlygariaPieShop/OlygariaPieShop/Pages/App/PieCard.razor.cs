9cc4d715f4e33ae9b86c9a54b67ad49a41a78b24
jvillad <jyvillad@gmail.com>
2023-03-26 11:53:15 +1100

    search feature using blazor instead of jquery

---
+++ OlygariaPieShop/OlygariaPieShop/Pages/App/PieCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using OlygariaPieShop.Models;
+
+namespace OlygariaPieShop.Pages.App
+{
+	public partial class PieCard
+	{
+		[Parameter]
+		public Pie? Pie { get; set; }
+	}
+}
